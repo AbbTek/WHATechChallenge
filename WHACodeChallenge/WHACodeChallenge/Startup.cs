9038d1ddb6f0ce85dc20a0d7d4e34e9f14d97090
Carlos Martínez Loyola <cmartinez@simplelabs.io>
2016-02-07 14:25:49 +1100

    Base project

---
+++ WHACodeChallenge/WHACodeChallenge/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WHACodeChallenge.Startup))]
+namespace WHACodeChallenge
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
